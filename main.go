601529fc158a7be0758e001286f996b39e9ecb8e
yanghuaiyuan1 <yanghuaiyuan1@jd.com>
2018-06-26 00:14:52 +0800

    增加 初始化代码

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/golangpkg/go-stat-reporter/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ tests/default_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"testing"
+)
+
+func init() {
+	println("#########")
+}
+
+// TestBeego is a sample to run an endpoint test
+func TestBeego(t *testing.T) {
+	println("########")
+}
