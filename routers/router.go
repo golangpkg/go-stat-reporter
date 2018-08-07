6b940ecacc1dd6ba7449135993fb47090be58a55
yhy363 <yhy363@yhy363.com>
2018-08-07 22:42:25 +0800

    增加 路由。修改页面样式。

--- routers/router.go
+++ routers/router.go
@@ -10,6 +10,6 @@ func init() {
 	beego.Router("/", &controllers.MainController{}, "get:Index")
 	beego.Router("/reporter/index", &controllers.MainController{}, "get:AdminIndex")
 	//主要报表数据接口。
-	beego.Router("/reporter/page/:pageId", &controllers.StatPageController{}, "get:PageHtml")
-	beego.Router("/reporter/table/api", &controllers.StatPageController{}, "get:TableApi")
+	beego.Router("/reporter/:pageId", &controllers.StatPageController{}, "get:PageHtml")
+	beego.Router("/reporter/api", &controllers.StatPageController{}, "get:TableApi")
 }
